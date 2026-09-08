4cac0c418778428a9b7d42c3ef3ebc5355a6cdd6
Harshita <apple@Apples-MacBook-Air.local>
2026-09-08 23:01:36 +0530

    Client ops done

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,6 +11,7 @@ const {notFound,errorHandler}= require("./middleware/errorHandler")
 
 const healthRouter=require("./routes/health")
 const authRouter=require("./routes/auth")
+const clientRouter=require("./routes/client")
 const app=express()
 
 
@@ -27,6 +28,7 @@ app.use(cookieParser());
 
 app.use("/api/health",healthRouter)
 app.use("/api/auth",authRouter);
+app.use("/api/clients",clientRouter);
 if (!env.isProd) app.use(morgan("dev"));
 
 app.use("/api/health", healthRouter);
